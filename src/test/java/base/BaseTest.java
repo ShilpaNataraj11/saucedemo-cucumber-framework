6a4e6721b70879e544eb58f966fb4e068bd678ce
ShilpaNataraj11 <shilpanataraj11@gmail.com>
2025-07-22 15:21:24 +0530

    Fix: Added headless Chrome for GitHub CI environment

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
 
@@ -10,7 +11,13 @@ public class BaseTest {
 
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");           // Use "new" headless mode (for Chrome v109+)
+        options.addArguments("--no-sandbox");             // Required for GitHub Actions runner
+        options.addArguments("--disable-dev-shm-usage");  // Prevent memory-related crashes
+
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
     }
